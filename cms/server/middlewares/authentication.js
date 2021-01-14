09850fa6853effadd54804c62f5acd9e2185678f
Muhammad Nur Hika Pasca <hikapasca@gmail.com>
2021-01-14 19:58:28 +0700

    - add Authentication include authentication

---
+++ cms/server/middlewares/authentication.js
@@ -0,0 +1,35 @@
+const { jwtVerify } = require("../helpers/jsonwebtoken");
+// const { User, Product } = require("../models");
+const UserModel = require("../models/User");
+const authentication = async function (req, res, next) {
+  try {
+    const access_token = req.headers.access_token;
+    if (!access_token) {
+      throw { message: "please login first!" };
+    } else {
+      const checkData = jwtVerify(access_token);
+      req.username = checkData.username;
+      next();
+    }
+  } catch (err) {
+    if (err.name === "JsonWebTokenError") {
+      return res.status(401).json({ message: "Please login first!" });
+    } else {
+      return res.status(500).json({ message: err });
+    }
+  }
+};
+
+const authorization = async function (req, res, next) {
+  try {
+    if (req.username !== "admin1223") {
+      return res.status(403).json({ message: "forbidden access" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
+};
+
+module.exports = { authentication, authorization };
