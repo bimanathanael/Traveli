7dd20df5663f60960639d22d75db804318c87e8e
Muhammad Nur Hika Pasca <hikapasca@gmail.com>
2021-01-14 19:59:27 +0700

    - add routes for:   - History   - User   - English   - Indonesia

---
+++ cms/server/routes/index.js
@@ -0,0 +1,41 @@
+const router = require("express").Router();
+const UserController = require("../controllers/UserController");
+const EnglishController = require("../controllers/EnglishController");
+const IndonesiaController = require("../controllers/IndonesiaController");
+const HistoryController = require("../controllers/HistoryController");
+const {
+  authentication,
+  authorization,
+} = require("../middlewares/authentication");
+
+router.post("/login", UserController.login);
+router.post("/register", authentication, authorization, UserController.addUser);
+router.get(
+  "/listAdmin",
+  authentication,
+  authorization,
+  UserController.readUser
+);
+router.delete(
+  "/:username",
+  authentication,
+  authorization,
+  UserController.deleteUser
+);
+router.put("/:username", authentication, UserController.updateUser);
+
+router.get("/en/", EnglishController.readData);
+router.get("/en/:pages", EnglishController.readOneData);
+router.get("/en/:pages/:content", EnglishController.readSpecifyData);
+router.post("/en", authentication, EnglishController.addData);
+router.put("/en/:pages", authentication, EnglishController.updateData);
+
+router.get("/id/", IndonesiaController.readData);
+router.get("/id/:pages", IndonesiaController.readOneData);
+router.post("/id", authentication, IndonesiaController.addData);
+router.put("/id/:pages", authentication, IndonesiaController.updateData);
+
+router.get("/history", authentication, HistoryController.readData);
+router.post("/history", authentication, HistoryController.addData);
+
+module.exports = router;
